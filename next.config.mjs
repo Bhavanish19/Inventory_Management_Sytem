6a50deefccca6c58688fac5f2064e3d08ab53a79
Bhavanish19 <brdhamnaskar@gmail.com>
2024-09-26 02:33:04 -0400

    m

---
+++ next.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    // images: {
+    //   remotePatterns: [
+    //     {
+    //       protocol: 'https',
+    //       hostname: 'https://inventorydashboard.s3.amazonaws.com', // Remove the protocol part
+    //       port: '', // This can be left as an empty string or omitted
+    //       pathname: '/**',
+    //     },
+    //   ],
+    // },
+  };
+  
+  export default nextConfig;
+  
